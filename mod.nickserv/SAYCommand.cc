01137a862db9fde5b8cb4647f19a62e6ffda8d94
jeekay <jeekay>
2002-02-04 04:26:36 +0000

    Fixed dumb bug where not finding the channel wouldnt stop SAY from echoing to it.

--- mod.nickserv/SAYCommand.cc
+++ mod.nickserv/SAYCommand.cc
@@ -3,7 +3,7 @@
  *
  * 20020130 - Jeekay - Initial version
  *
- * $Id: SAYCommand.cc,v 1.1 2002-01-30 21:57:54 jeekay Exp $
+ * $Id: SAYCommand.cc,v 1.2 2002-02-04 04:26:36 jeekay Exp $
  */
 
 #include <string>
@@ -42,6 +42,7 @@ Channel* theChan = Network->findChannel(st[1]);
 if(!theChan)
 	{
 	bot->Notice(theClient, "Channel not found");
+	return false;
 	} // Channel doesnt exist
 
 bot->Message(st[1], st.assemble(2).c_str());
