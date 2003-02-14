e7e751e7b3a84f4bd9e29bae0de0f932d09f115f
jeekay <jeekay>
2003-02-14 15:28:55 +0000

    Fix minor compile issues

--- mod.nickserv/nickserv.cc
+++ mod.nickserv/nickserv.cc
@@ -5,7 +5,8 @@
 #include	<algorithm>
 
 #include	<cstring>
-#include <time.h>
+#include	<time.h>
+#include	<stdarg.h>
 
 #include	"client.h"
 #include	"iClient.h"
@@ -22,7 +23,7 @@
 #include	"server.h"
 
 const char Nickserv_h_rcsId[] = __NICKSERV_H ;
-const char Nickserv_cc_rcsId[] = "$Id: nickserv.cc,v 1.37 2002-10-11 14:07:48 jeekay Exp $" ;
+const char Nickserv_cc_rcsId[] = "$Id: nickserv.cc,v 1.38 2003-02-14 15:28:55 jeekay Exp $" ;
 
 // If __NS_DEBUG is defined, no output is ever sent to users
 // this also prevents users being killed. It is intended
