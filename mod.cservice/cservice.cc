7e27502f581ddfc9221a164c94d4cbf1ce54c96b
jeekay <jeekay>
2004-08-19 18:41:24 +0000

    Version bump to 1.2.17

--- mod.cservice/cservice.cc
+++ mod.cservice/cservice.cc
@@ -708,7 +708,7 @@ else if(Command == "VERSION")
 	xClient::DoCTCP(theClient, CTCP,
 		"NetGamers P10 Channel Services II ["
 		__DATE__ " " __TIME__
-		"] Release 1.2.16");
+		"] Release 1.2.17");
 	}
 else if(Command == "DCC")
 	{
